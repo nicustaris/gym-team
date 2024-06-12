e0205f61a0990ee7874a73956f4de2526b00eb5d
nicustaris <nicolae.staris@gmail.com>
2024-06-12 23:37:53 +0100

    config update

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/gym-team/",
-  build: {
-    rollupOptions: {
-      external: ["react-slick"],
-    },
-  },
 });
