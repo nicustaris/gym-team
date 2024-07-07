af70bc3ff2d77e052198805ecf886c7e38cdd67b
nicustaris <nicolae.staris@gmail.com>
2024-07-07 22:39:28 +0100

    Review slider UI

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 
 import "./Reviews.scss";
+import profile from "./profile.avif";
+
+import { IoMdQuote } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 const Reviews = () => {
   return (
     <div className="reviews wrapper">
       <h1>Reviews From Clients</h1>
       <div className="reviews__container">
-        <div className="reviews__container__reviews">d</div>
+        <div className="reviews__container__slider">
+          <div className="reviews__container__slider__header">
+            <div className="reviews__container__slider__header__quote">
+              <IoMdQuote />
+            </div>
+            <div className="reviews__container__slider__header__page">1/4</div>
+          </div>
+          <div className="reviews__container__slider__review">
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+              voluptatibus adipisci consequatur aspernatur dignissimos in.
+            </h3>
+          </div>
+          <div className="reviews__container__slider__author">
+            <div className="reviews__container__slider__author__profile">
+              <img src={profile} alt="/" />
+              <div className="reviews__container__slider__author__profile__details">
+                <h4>Albert Flores</h4>
+                <p>Profi coach</p>
+              </div>
+            </div>
+            <div className="reviews__container__slider__author__profile__arrows">
+              <IoIosArrowDropleftCircle />
+              <IoIosArrowDroprightCircle />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
