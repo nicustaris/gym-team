f3203165e0a658708ea3520f286510f02b886a33
nicustaris <nicolae.staris@gmail.com>
2024-07-28 18:34:43 +0100

    polishing blogs section

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import "./Blogs.scss";
 
 import Slider from "react-slick";
+import { FiArrowUpRight } from "react-icons/fi";
+import background from "./bg.jpg";
 
 const Blogs = () => {
   const blogs = [
@@ -19,7 +21,20 @@ const Blogs = () => {
         animi libero suscipit aut, earum doloremque consequuntur iusto adipisci,
         eaque ipsa atque. Molestias, quia!
       </p>
-      <div className="blog__slider"></div>
+      <div className="blog__slider">
+        <img src={background} alt="" className="blog__slider__img" />
+        <button className="blog__slider__button">New Zone</button>
+        <p className="blog__slider__paragraph">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non vero
+          cupiditate facilis eligendi?
+        </p>
+        <div className="blog__slider__info">
+          <button>Read More</button>
+          <span>
+            <FiArrowUpRight size={20} />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
