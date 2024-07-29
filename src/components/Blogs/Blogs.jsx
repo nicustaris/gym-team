525f534ea89bf5bccd02cfe7ade509a1847d2046
nicustaris <nicolae.staris@gmail.com>
2024-07-29 19:39:23 +0100

    blogs section polishing

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -4,35 +4,95 @@ import "./Blogs.scss";
 
 import Slider from "react-slick";
 import { FiArrowUpRight } from "react-icons/fi";
+
+// Slider images
 import background from "./bg.jpg";
+import background2 from "./bg2.jpg";
+import background3 from "./bg3.jpg";
 
 const Blogs = () => {
-  const blogs = [
+  const blogsList = [
     {
-      area: "",
-      description: "",
+      area: "New Zone",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non vero cupiditate facilis eligendi?",
+      image: background,
+    },
+    {
+      area: "New Zone",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non vero cupiditate facilis eligendi?",
+      image: background2,
+    },
+    {
+      area: "New Zone",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non vero cupiditate facilis eligendi?",
+      image: background3,
     },
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Display 3 items at a time
+    slidesToScroll: 1,
+    // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1200, // Large screen (laptop and above)
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768, // Medium devices (tablets, small laptops)
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576, // Small devices (phones)
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="blog wrapper">
-      <h1 className="blog__heading">Blog & News</h1>
-      <p className="blog__text">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
-        animi libero suscipit aut, earum doloremque consequuntur iusto adipisci,
-        eaque ipsa atque. Molestias, quia!
-      </p>
-      <div className="blog__slider">
-        <img src={background} alt="" className="blog__slider__img" />
-        <button className="blog__slider__button">New Zone</button>
-        <p className="blog__slider__paragraph">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non vero
-          cupiditate facilis eligendi?
+    <div className="blog__container">
+      <div className="blog wrapper">
+        <h1 className="blog__heading">Blog & News</h1>
+        <p className="blog__text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
+          animi libero suscipit aut, earum doloremque consequuntur iusto
+          adipisci, eaque ipsa atque. Molestias, quia!
         </p>
-        <div className="blog__slider__info">
-          <button>Read More</button>
-          <span>
-            <FiArrowUpRight size={20} />
-          </span>
+        <div className="sliders__container">
+          <Slider {...settings}>
+            {blogsList.map((blog) => (
+              <div className="blog__slider">
+                <div className="blog__slider__image">
+                  <img
+                    src={blog.image}
+                    alt=""
+                    className="blog__slider__image__img"
+                  />
+                </div>
+
+                <button className="blog__slider__button">{blog.area}</button>
+                <p className="blog__slider__paragraph">{blog.description}</p>
+                <div className="blog__slider__info">
+                  <button>Read More</button>
+                  <span>
+                    <FiArrowUpRight size={20} />
+                  </span>
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
