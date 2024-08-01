16de103cc8995fe211aaa4fba7d63dca02c061f5
nicustaris <nicolae.staris@gmail.com>
2024-08-01 22:46:19 +0100

    Trial Visit Section Added

---
+++ src/components/TrialVisit/TrialVisit.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./TrialVisit.scss";
+
+const TrialVisit = () => {
+  return (
+    <div className="trial__container">
+      <h1>Get Your First Trial Visit</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit
+        voluptatibus placeat error ut debitis qui, similique quisquam cumque
+        maiores dolorem? Error, soluta ratione.
+      </p>
+      <div className="trial__container__submit">
+        <input type="text" />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default TrialVisit;
