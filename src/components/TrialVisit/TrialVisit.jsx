f242126a2d860143a5a454ad777a6f6c8c67b045
nicustaris <nicolae.staris@gmail.com>
2024-08-04 19:49:26 +0100

    Trial Visit finishingF

--- src/components/TrialVisit/TrialVisit.jsx
+++ src/components/TrialVisit/TrialVisit.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
+import background from "./background.jpg";
+
 import "./TrialVisit.scss";
 
 const TrialVisit = () => {
   return (
     <div className="trial__container">
-      <h1>Get Your First Trial Visit</h1>
-      <p>
+      <h1 className="trial__container__heading">Get Your First Trial Visit</h1>
+      <p className="trial__container__paragraph">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit
         voluptatibus placeat error ut debitis qui, similique quisquam cumque
         maiores dolorem? Error, soluta ratione.
       </p>
       <div className="trial__container__submit">
-        <input type="text" />
+        <input type="text" placeholder="sample@example.com" />
         <button>Submit</button>
       </div>
     </div>
