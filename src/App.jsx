b3961b998f71cfdf57375a53cee1d4ff9ff67ce8
nicustaris <nicolae.staris@gmail.com>
2024-08-08 20:58:16 +0100

    Adding Routes and some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
-import About from "./components/About/About";
-import Blogs from "./components/Blogs/Blogs";
-import ChooseUs from "./components/ChooseUs/ChooseUs";
-import Coaches from "./components/Coaches/Coaches";
-import Footer from "./components/Footer/Footer";
-import GymPasses from "./components/GymPasses/GymPasses";
-import Hero from "./components/Hero/Hero";
-import HighlightImage from "./components/HighlightImage/HighlightImage";
-import Reviews from "./components/Reviews/Reviews";
-import Timetable from "./components/Timetable/Timetable";
-import TrialVisit from "./components/TrialVisit/TrialVisit";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <>
-      <Hero />
-      <ChooseUs />
-      <About />
-      <Coaches />
-      <Timetable />
-      <HighlightImage />
-      <GymPasses />
-      <Reviews />
-      <Blogs />
-      <TrialVisit />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
