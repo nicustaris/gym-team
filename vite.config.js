a980aa589f2fa68149026c9aff811ff8d04dc237
nicustaris <nicolae.staris@gmail.com>
2024-08-12 23:21:32 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Access environment variable using process.env
 const baseURL = process.env.VITE_BASE_URL || "/";
 
 export default defineConfig({
