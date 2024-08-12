1bbe238650332720291813fd82aa02ecdeb99eb7
nicustaris <nicolae.staris@gmail.com>
2024-08-12 20:53:34 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/AppRoutes";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return <Footer />;
 }
 
 export default App;
