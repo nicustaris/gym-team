0e99f9c2c5b29c722da477b2bec05b3f8ed41c98
nicustaris <nicolae.staris@gmail.com>
2024-08-12 19:49:06 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/gym-team" : "/",
+  base: process.env.VITE_BASE_URL === "production" ? "/gym-team" : "/",
   // base: process.env.VITE_BASE_URL || '/',
 });
