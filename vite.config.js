f7c0a5dc3c9c02826f5f64d2e958e6bdaf6e7c48
nicustaris <nicolae.staris@gmail.com>
2024-08-12 23:50:38 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Access environment variable using import.meta.env
+const baseURL = process.env.VITE_BASE_URL || "/";
+
+console.log("Base URL in Vite Config:", baseURL);
+
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || "/",
+  base: baseURL,
 });
