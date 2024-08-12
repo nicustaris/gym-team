cc7fcab85bb2c5501b0cea2ef0fdd46da4f3bb9b
nicustaris <nicolae.staris@gmail.com>
2024-08-12 23:18:26 +0100

    update vite config

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 
+console.log("Base URL in App:", import.meta.env.VITE_BASE_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Access environment variable using process.env
 const baseURL = process.env.VITE_BASE_URL || "/";
 
-console.log("HEY");
-console.log(baseURL);
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: baseURL,
