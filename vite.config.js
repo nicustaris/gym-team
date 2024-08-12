77272cc99c43552d63481ed73120776aafd3a933
nicustaris <nicolae.staris@gmail.com>
2024-08-12 23:37:20 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const baseURL = process.env.VITE_BASE_URL || "/";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/gym-team/",
+  base: process.env.VITE_BASE_URL || "/",
 });
