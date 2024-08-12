3e15ae90092a8b9940baba94fb39cabf88823b27
nicustaris <nicolae.staris@gmail.com>
2024-08-12 23:59:56 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-console.log("VITE_BASE_URL:", process.env.VITE_BASE_URL);
-console.log("import.meta.env.VITE_BASE_URL:", import.meta.env.VITE_BASE_URL);
-
-const baseURL = import.meta.env.VITE_BASE_URL || "/";
-
 export default defineConfig({
   plugins: [react()],
   base: "/gym-team/",
