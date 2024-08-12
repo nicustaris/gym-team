88cc49ea3b526a6e5ce7595c95958ea223374c6e
nicustaris <nicolae.staris@gmail.com>
2024-08-12 23:11:13 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const baseURL = process.env.VITE_BASE_URL || "/";
+
+console.log("HEY");
+console.log(baseURL);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || "/",
+  base: baseURL,
 });
