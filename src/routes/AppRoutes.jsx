2e064db4676f7e433ea7ca03fdb67e91444ac540
nicustaris <nicolae.staris@gmail.com>
2024-08-13 22:25:22 +0100

    update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Home from "../pages/Home/Home";
 import Shop from "../pages/Shop/Shop";
+import NotFound from "./../pages/NotFound/NotFound";
 
 const routes = [
   {
@@ -10,6 +12,10 @@ const routes = [
   {
     path: "/shop",
     element: <Shop />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ];
 
