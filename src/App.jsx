0c9cbf8866d848c33a80c8c2eb9f15e1e6a031ef
nicustaris <nicolae.staris@gmail.com>
2024-08-13 20:00:31 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import router from "./routes/AppRoutes";
 import Home from "./pages/Home/Home";
