c5221629c306db6d781cb28b5f703df21db8de3d
nicustaris <nicolae.staris@gmail.com>
2024-08-14 21:38:47 +0100

    Routes update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 
 import router from "./routes/AppRoutes";
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  // return <RouterProvider router={router} />;
 }
 
 export default App;
