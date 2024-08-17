a72b85f49488fa8acb1898b4ab7cf381b77cc5d0
nicustaris <nicolae.staris@gmail.com>
2024-08-17 20:53:48 +0100

    Shop & Pagination done

---
+++ src/pages/Shop/Pagination.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import "../../styles/_mixins.scss";
+
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+const Pagination = ({
+  totalProducts,
+  productsPerPage,
+  currentPage,
+  setCurrentPage,
+}) => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [currentPage]);
+
+  let pages = [];
+  const totalPages = Math.ceil(totalProducts / productsPerPage);
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  return (
+    <>
+      <div className="shop__page__content__pagination">
+        <MdKeyboardArrowLeft size={20} onClick={prevPage} />
+        {pages.length > 1 && (
+          <ul>
+            {pages.map((page) => (
+              <li key={page}>
+                <button
+                  className={`${page === currentPage ? `active` : ""}`}
+                  onClick={() => setCurrentPage(page)}
+                >
+                  {page}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+        <MdKeyboardArrowRight size={20} onClick={nextPage} />
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -8,17 +8,29 @@ import { FiArrowUpRight } from "react-icons/fi";
 import product from "./product.png";
 
 import "./Shop.scss";
+import Pagination from "./Pagination";
+import Footer from "../../components/Footer/Footer";
 
 const Shop = () => {
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(500);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleInput = (e) => {
     setMinPrice(e.minValue);
     setMaxPrice(e.maxValue);
   };
 
-  const products = new Array(6).fill("");
+  const products = new Array(27).fill("");
+
+  // Pagination logic
+  const productsPerPage = 6;
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
 
   return (
     <>
@@ -75,32 +87,37 @@ const Shop = () => {
           </div>
         </div>
         <div className="shop__page__content">
-          {products.map(() => (
-            <div className="shop__page__content__card">
-              <img
-                src={product}
-                alt=""
-                className="shop__page__content__card__img"
-              />
-              <span className="shop__page__content__card__category">
-                Gainers
-              </span>
-              <h3 className="shop__page__content__card__name">Mega Mass</h3>
-              <div className="shop__page__content__card__price">
-                <span className="old-price">£130</span>
-                <span className="price">£120</span>
+          <div className="shop__page__content__products">
+            {currentProducts.map((_, i) => (
+              <div key={i} className="shop__page__content__products__card">
+                <img
+                  src={product}
+                  alt=""
+                  className="shop__page__content__products__card__img"
+                />
+                <span className="shop__page__content__products__card__category">
+                  Gainers
+                </span>
+                <h3 className="shop__page__content__products__card__name">
+                  Mega Mass
+                </h3>
+                <div className="shop__page__content__products__card__price">
+                  <span className="old-price">£130</span>
+                  <span className="price">£120</span>
+                </div>
               </div>
-            </div>
-          ))}
-          <div className="shop__page__content__pagination">
-            <button>{"<"}</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>{">"}</button>
+            ))}
           </div>
+
+          <Pagination
+            totalProducts={products.length}
+            productsPerPage={6}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
         </div>
       </div>
+      <Footer />
     </>
   );
 };
