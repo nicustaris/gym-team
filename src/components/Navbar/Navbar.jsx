1a0293a00ec8918a4ceea9a8aad9c461152011ae
nicustaris <nicolae.staris@gmail.com>
2024-08-19 00:01:15 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import "./Navbar.scss";
+import { NavLink } from "react-router-dom";
 
 import { CiSearch } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { Link, NavLink } from "react-router-dom";
+import { MdClose } from "react-icons/md";
+
+import "./Navbar.scss";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -16,61 +18,71 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar wrapper">
-      <h2>GYMTEAM</h2>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/shop"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Shop
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/blog"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Blog
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
-              Contact
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="navbar__icons__container">
-        <div className="search_icon">
-          <CiSearch size={21} />
+    <div className="navbar-wrapper">
+      <div className="navbar wrapper">
+        <h2>GYMTEAM</h2>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/shop"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Shop
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/blog"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="navbar__icons__container">
+          <div className="search_icon">
+            <CiSearch size={21} />
+          </div>
+          <CiMail size={21} />
+          {!isMenuOpen ? (
+            <RxHamburgerMenu
+              size={21}
+              onClick={toggleMenu}
+              className="navbar__icons__container__hamburgerMenu"
+            />
+          ) : (
+            <MdClose
+              size={21}
+              onClick={toggleMenu}
+              className="navbar__icons__container__hamburgerMenuClose"
+            />
+          )}
         </div>
-        <CiMail size={21} />
-        <RxHamburgerMenu
-          size={21}
-          onClick={toggleMenu}
-          className="navbar__icons__container__hamburger"
-        />
-      </div>
 
-      {isMenuOpen && (
-        <div className="navbar__overlay">
-          <span>hey</span>
-        </div>
-      )}
+        {isMenuOpen && (
+          <div className="navbar__overlay">
+            <span>hey</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
