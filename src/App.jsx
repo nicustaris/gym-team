f29d83d072d2fe17b7662b3d026ef976b5f105e6
nicustaris <nicolae.staris@gmail.com>
2024-09-21 18:54:23 +0100

    Redux store added and some modification to the User Slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/AppRoutes";
+import { supabase } from "./utils/supabase";
+import { useDispatch } from "react-redux";
+import { clearCurrentUser, setCurrentUser } from "./store/user/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.user) {
+        dispatch(setCurrentUser(session.user));
+      } else {
+        dispatch(clearCurrentUser());
+      }
+    });
+
+    // Clean up the auth listener
+    return () => {
+      if (data) {
+        data?.subscription?.unsubscribe();
+      }
+    };
+  }, [dispatch]);
+
   return (
     <>
       <RouterProvider router={router} />
